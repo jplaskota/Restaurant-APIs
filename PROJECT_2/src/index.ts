833874e12d9f1c43ae6186d8795c77a6604e7325
jurandplaskota <jurand.plaskota@microsoft.wsei.edu.pl>
2022-03-17 16:31:35 +0100

    find by id done

--- PROJECT_2/src/index.ts
+++ PROJECT_2/src/index.ts
@@ -1,23 +1,50 @@
 import express from 'express'
 import {Request, Response} from 'express'
+import { parse } from 'path'
 import { Note } from './note'
 
 const app = express()
-
-const notes: Note[] = []
-
-const addNote = (newNote: Note) => {
-    notes.push(newNote)};
 
 app.use(express.json())
 
 app.get('/', function (req: Request, res: Response) {
   res.send('GET Hello World')
 })
+
 app.post('/', function (req: Request, res: Response) {
   console.log(req.body) // e.x. req.body.title 
   res.status(200).send('POST Hello World')
 })
+
+const notes: Note[] = [
+  {title: 'a', content: 'one', createDate: "1-01-2022", tags: ['a','b','c'], id: 1 },
+  {title: 'b', content: 'two', createDate: "2-02-2022", tags: ['a','c'], id: 2 }
+]
+
+function addNote(newNote: Note){
+  notes.push(newNote)
+}
+
+function search(a: number){
+  for(const item of notes){
+    if(item.id === a){
+      return true
+    }
+  }
+  return false
+}
+
+app.get('/note/:id', function (req: Request, res: Response) {
+  const ID = parseInt(req.params.id)
+  if(search(ID)){
+    res.status(201).send(notes.find(a => a.id === ID))
+  }
+  else{
+    res.status(404).send("not found")
+  }
+})
+
+// do zrobienia
 app.post('/note', (req, res) => {
     const title = req.body.title;
     const content = req.body.content;
