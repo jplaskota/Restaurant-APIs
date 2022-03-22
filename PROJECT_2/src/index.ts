caa6088d90758254f01af68a32dac794cd09efae
axlcra <jurand.plaskota@gmail.com>
2022-03-22 01:28:54 +0100

    project 2 done

--- PROJECT_2/src/index.ts
+++ PROJECT_2/src/index.ts
@@ -1,61 +1,93 @@
-import express from 'express'
-import {Request, Response} from 'express'
-import { parse } from 'path'
-import { Note } from './note'
+import express from "express";
+import { Request, Response } from "express";
+import { parse } from "path";
+import { Note } from "./note";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', function (req: Request, res: Response) {
-  res.send('GET Hello World')
-})
+app.get("/", function (req: Request, res: Response) {
+  res.send("GET Hello World");
+});
 
-app.post('/', function (req: Request, res: Response) {
-  console.log(req.body) // e.x. req.body.title 
-  res.status(200).send('POST Hello World')
-})
+app.post("/", function (req: Request, res: Response) {
+  console.log(req.body); // e.x. req.body.title
+  res.status(200).send("POST Hello World");
+});
 
 const notes: Note[] = [
-  {title: 'a', content: 'one', createDate: "1-01-2022", tags: ['a','b','c'], id: 1 },
-  {title: 'b', content: 'two', createDate: "2-02-2022", tags: ['a','c'], id: 2 }
-]
+  {
+    title: "a",
+    content: "one",
+    createDate: "1-01-2022",
+    tags: ["a", "b", "c"],
+    id: 1,
+  },
+  {
+    title: "b",
+    content: "two",
+    createDate: "2-02-2022",
+    tags: ["a", "c"],
+    id: 2,
+  },
+];
 
-function addNote(newNote: Note){
-  notes.push(newNote)
-}
-
-function search(a: number){
-  for(const item of notes){
-    if(item.id === a){
-      return true
-    }
+app.get("/note/:id", function (req: Request, res: Response) {
+  const note = notes.find((a) => a.id === parseInt(req.params.id));
+  if (note) {
+    res.status(200).send(note);
+  } else {
+    res.status(404).send("Not Found");
   }
-  return false
-}
+});
 
-app.get('/note/:id', function (req: Request, res: Response) {
-  const ID = parseInt(req.params.id)
-  if(search(ID)){
-    res.status(201).send(notes.find(a => a.id === ID))
+app.post("/note", (req: Request, res: Response) => {
+  if (req.body.title && req.body.content) {
+    const note: Note = {
+      title: req.body.title,
+      content: req.body.content,
+      createDate: new Date().toISOString(),
+      tags: req.body.tags,
+      id: notes.length + 1,
+    };
+
+    notes.push(note);
+    res.sendStatus(201);
+  } else {
+    res.status(400).send("Not Created");
   }
-  else{
-    res.status(404).send("not found")
+});
+
+app.put("/note/:id", (req: Request, res: Response) => {
+  const note = notes.find((a) => a.id === parseInt(req.params.id));
+  if (!note) {
+    res.status(404).send("Not Found");
   }
-})
+  else {
+    const newnote: Note = {
+      title: req.body.title,
+      content: req.body.content,
+      createDate: note.createDate ?? new Date().toISOString(),
+      tags: !req.body.tags ? note.tags : req.body.tags,
+      id: note.id,
+    };
+    const index = notes.indexOf(note);
+    notes[index] = newnote;
+    res.status(204).send("Updated");
+  }
+});
 
-// do zrobienia
-app.post('/note', (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
+app.delete("/note/:id", (req: Request, res: Response) => {
+  const note = notes.find((a) => a.id === parseInt(req.params.id));
+  if (!note) {
+    res.status(400).send("Not Found");
+  }
+  else {
+    const index = notes.indexOf(note);
+    notes.splice(index, 1);
+    res.status(204).send("Deleted");
+  }
+});
 
-    if (title?.trim() || content?.trim()) {
-      return res.status(400).send('Title or Content');
-    }
-
-    const date = new Date()
-    addNote({ title, content, createDate : date.toISOString(), tags : new Array("one", "two", "three"), id : Date.now()});
-    res.status(201).send('Note created');
-  });
-
-app.listen(3000)
+app.listen(3000);
